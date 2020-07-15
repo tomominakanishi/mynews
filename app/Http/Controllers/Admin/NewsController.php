2aab2c4f887d4ba898f69700f41b95a43894e8d7
tomominakanishi <tomomin1030@gmail.com>
2020-07-16 08:52:25 +0900

    20th commit

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -14,6 +14,7 @@ class NewsController extends Controller
   {
     return view('admin.news.create');
   }
+  
   public function create(Request $request)
   {
     $this->validate($request, News::$rules);
@@ -38,36 +39,32 @@ class NewsController extends Controller
 
   public function index(Request $request)
   {
-    $cond_title = $request->cond_title;
-    if ($cond_title != ''){
-      $posts = News::where('title', $cond_title)->get();
-    } else {
-      $posts = News::all();
-;
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          $posts = News::where('title', $cond_title)->get();
+      } else {
+          $posts = News::all();
+      }
+      return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
   }
+
 
   public function edit(Request $request)
   {
-    $news = News::find($request->id);
-    if (empty($news)){
-      abort(404);
-    }
-    return view('admin.news.edit', ['news_form' => $news]);
+      $news = News::find($request->id);
+      if (empty($news)) {
+        abort(404);    
+      }
+      return view('admin.news.edit', ['news_form' => $news]);
   }
 
-  public function update(Request $request)
-  {
-    $this->validate($request, News::$rules);
-    $news = News::find($request->id);
+
+  public function update(Request $request)  {
+    $this->validate($request, News::$rules);    $news = News::find($request->id);
     $news_form = $request->all();
      if ($request->remove == 'true') {
-       $news_form['image_path'] = null;
-     } elseif ($request->file('image')) {
-       $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
-       $news->image_path = Storage::disk('s3')->url($path);
-     } else {
-       $news_form['image_path'] = $news->image_path;
-     }
+       $news_form['image_path'] = null;     } elseif ($request->file('image')) {       $path = Storage::disk('s3')->putFile('/',$form['image'],'public');       $news->image_path = Storage::disk('s3')->url($path);     } else {
+       $news_form['image_path'] = $news->image_path;     }
 
      unset($news_form['_token']);
      unset($news_form['image']);
@@ -76,18 +73,15 @@ class NewsController extends Controller
 
      $history = new History;
      $history->news_id = $news->id;
-     $history->edited_at = Carbon::now();
-     $history->save();
+     $history->edited_at = Carbon::now();     $history->save();
 
      return redirect('admin/news/');
   }
 
-  public function delete(Request $request)
-  {
+  public function delete(Request $request)  {
     $news = News::find($request->id);
 
     $news->delete();
     return redirect('admin/news/');
   }
 }
-
